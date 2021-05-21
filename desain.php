f0d47c22b29458dfdebae092a73cc605fefb17dc
mGatut <76194366+mGatut@users.noreply.github.com>
2021-05-21 09:38:52 +0700

    Terbaru dari Chindy

---
+++ desain.php
@@ -0,0 +1,306 @@
+<!DOCTYPE html>
+<html lang="en">
+   <head>
+      <!-- basic -->
+      <meta charset="utf-8">
+      <meta http-equiv="X-UA-Compatible" content="IE=edge">
+      <!-- mobile metas -->
+      <meta name="viewport" content="width=device-width, initial-scale=1">
+      <meta name="viewport" content="initial-scale=1, maximum-scale=1">
+      <!-- site metas -->
+      <title>YourArchi.id</title>
+      <meta name="keywords" content="">
+      <meta name="description" content="">
+      <meta name="author" content="">
+      <!-- bootstrap css -->
+      <link rel="stylesheet" href="css/bootstrap.min.css">
+      <!-- style css -->
+      <link rel="stylesheet" href="css/style.css">
+      <!-- Responsive-->
+      <link rel="stylesheet" href="css/responsive.css">
+      <!-- fevicon -->
+      <link rel="icon" href="images/fevicon.png" type="image/gif" />
+      <!-- Scrollbar Custom CSS -->
+      <link rel="stylesheet" href="css/jquery.mCustomScrollbar.min.css">
+      <!-- Tweaks for older IEs-->
+      <link rel="stylesheet" href="https://netdna.bootstrapcdn.com/font-awesome/4.0.3/css/font-awesome.css">
+      <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/fancybox/2.1.5/jquery.fancybox.min.css" media="screen">
+      <!--[if lt IE 9]>
+      <script src="https://oss.maxcdn.com/html5shiv/3.7.3/html5shiv.min.js"></script>
+      <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script><![endif]-->
+   </head>
+   <!-- body -->
+   <body class="main-layout product_page">
+      <!-- loader  -->
+      <div class="loader_bg">
+         <div class="loader"><img src="images/loading.gif" alt="#" /></div>
+      </div>
+      <!-- end loader --> 
+      <!-- header -->
+      <header>
+         <!-- header inner -->
+         <div class="header">
+            <div class="head_top">
+               <div class="container">
+                  <div class="row">
+                    <div class="col-xl-6 col-lg-6 col-md-6 col-sm-12">
+                       <div class="top-box">
+                        <ul class="sociel_link">
+                         <!-- <li> <a href="#"><i class="fa fa-facebook-f"></i></a></li>
+                         <li> <a href="#"><i class="fa fa-twitter"></i></a></li>
+                         <li> <a href="#"><i class="fa fa-instagram"></i></a></li>
+                         <li> <a href="#"><i class="fa fa-linkedin"></i></a></li> -->
+                     </ul>
+                    </div>
+                  </div>
+                  <div class="col-xl-6 col-lg-6 col-md-6 col-sm-12">
+                       <div class="top-box">
+                        <p></p>
+                    </div>
+                  </div>
+               </div>
+            </div>
+         </div>
+         <div class="container">
+            <div class="row">
+               <div class="col-xl-3 col-lg-3 col-md-3 col-sm-3 col logo_section">
+                  <div class="full">
+                     <div class="center-desk">
+                        <div class="logo"> <a href="index.php"><img src="images/logo.jpeg" alt="logo"/></a> </div>
+                     </div>
+                  </div>
+               </div>
+               <div class="col-xl-7 col-lg-7 col-md-9 col-sm-9">
+                  <div class="menu-area">
+                     <div class="limit-box">
+                        <nav class="main-menu">
+                           <ul class="menu-area-main">
+                              <li > <a href="index.php">Home</a> </li>
+                              <li> <a href="about.php">About</a> </li>
+                              <li class="active"> <a href="desain.php">Design</a> </li>
+                              <li> <a href="desainer.php">Designer</a> </li>
+                              <li class="mean-last"> <a href="signup.php">Signup</a> </li>
+                               
+                           </ul>
+                        </nav>
+                     </div>
+                  </div>
+               </div>
+               <div class="col-xl-2 col-lg-2 col-md-2 col-sm-2">
+                  <li><a class="buy" href="login.php">Login</a></li>
+               </div>
+            </div>
+         </div>
+         <!-- end header inner --> 
+      </header>
+      <!-- end header -->
+       <div class="brand_color">
+        <div class="container">
+            <div class="row">
+                <div class="col-md-12">
+                    <div class="titlepage">
+                        <h2>design</h2>
+                    </div>
+                </div>
+            </div>
+        </div>
+
+    </div>
+      <!-- our product -->
+      <div class="product">
+         <div class="container">
+            <div class="row">
+               <div class="col-md-12">
+                  <div class="title">
+                     <span>We make the best design to make you a happy customer.</span>
+                  </div>
+               </div>
+            </div>
+         </div>
+      </div>
+      <div class="product-bg">
+         <div class="product-bg-white">
+         <div class="container">
+            <div class="row">
+               <div class="col-xl-3 col-lg-3 col-md-6 col-sm-12">
+                  <div class="product-box">
+                     <i> <a href="order.php"><img src="images/desain7.jpeg"/></a></i>
+                     <h3>Design A</h3>
+                     <span>$25.00</span>
+                  </div>
+               </div>
+               <div class="col-xl-3 col-lg-3 col-md-6 col-sm-12">
+                  <div class="product-box">
+                     <i><a href="order.php"><img src="images/desain12.jpeg"/></a></i>
+                     <h3>Design B</h3>
+                     <span>$25.00</span>
+                  </div>
+               </div>
+               <div class="col-xl-3 col-lg-3 col-md-6 col-sm-12">
+                  <div class="product-box">
+                     <i><a href="order.php"><img src="images/desain2.jpeg"/></a></i>
+                     <h3>Design C</h3>
+                     <span>$25.00</span>
+                  </div>
+               </div>
+               <div class="col-xl-3 col-lg-3 col-md-6 col-sm-12">
+                  <div class="product-box">
+                     <i><a href="order.php"><img src="images/desain1.jpeg"/></a></i>
+                     <h3>Design D</h3>
+                     <span>$25.00</span>
+                  </div>
+               </div>
+               <div class="col-xl-3 col-lg-3 col-md-6 col-sm-12">
+                  <div class="product-box">
+                     <i><a href="order.php"><img src="images/desain5.jpeg"/></a></i>
+                     <h3>Design E</h3>
+                     <span>$25.00</span>
+                  </div>
+               </div>
+               <div class="col-xl-3 col-lg-3 col-md-6 col-sm-12">
+                  <div class="product-box">
+                     <i><a href="order.php"><img src="images/desain6.jpeg"/></a></i>
+                     <h3>Design F</h3>
+                     <span>$25.00</span>
+                  </div>
+               </div>
+               <div class="col-xl-3 col-lg-3 col-md-6 col-sm-12">
+                  <div class="product-box">
+                     <i><a href="order.php"><img src="images/desain4.jpeg"/></a></i>
+                     <h3>Design G</h3>
+                     <span>$25.00</span>
+                  </div>
+               </div>
+               <div class="col-xl-3 col-lg-3 col-md-6 col-sm-12">
+                  <div class="product-box">
+                     <i><a href="order.php"><img src="images/desain9.jpeg"/></a></i>
+                     <h3>Design H</h3>
+                     <span>$25.00</span>
+                  </div>
+               </div>
+               <div class="col-xl-3 col-lg-3 col-md-6 col-sm-12">
+                  <div class="product-box">
+                     <i><a href="order.php"><img src="images/desain10.jpeg"/></a></i>
+                     <h3>Design I</h3>
+                     <span>$25.00</span>
+                  </div>
+               </div>
+               <div class="col-xl-3 col-lg-3 col-md-6 col-sm-12">
+                  <div class="product-box">
+                     <i><a href="order.php"><img src="images/desain3.jpeg"/></a></i>
+                     <h3>Design J</h3>
+                     <span>$25.00</span>
+                  </div>
+               </div>
+               <div class="col-xl-3 col-lg-3 col-md-6 col-sm-12">
+                  <div class="product-box">
+                     <i><a href="order.php"><img src="images/desain8.jpeg"/></a></i>
+                     <h3>Design K</h3>
+                     <span>$25.00</span>
+                  </div>
+               </div>
+               <div class="col-xl-3 col-lg-3 col-md-6 col-sm-12">
+                  <div class="product-box">
+                     <i><a href="order.php"><img src="images/desain11.jpeg"/></a></i>
+                     <h3>Design L</h3>
+                     <span>$25.00</span>
+                  </div>
+               </div>
+               </div>
+            </div>
+         </div>
+         
+      <!--  footer --> 
+      <footr>
+         <!-- <div class="footer">
+            <div class="container">
+               <div class="row">
+                  <div class="col-md-6 offset-md-3">
+                     <ul class="sociel">
+                         <li> <a href="#"><i class="fa fa-facebook-f"></i></a></li>
+                         <li> <a href="#"><i class="fa fa-twitter"></i></a></li>
+                         <li> <a href="#"><i class="fa fa-instagram"></i></a></li>
+                         <li> <a href="#"><i class="fa fa-instagram"></i></a></li>
+                     </ul>
+                  </div>
+            </div>
+            <div class="row">
+               <div class="col-xl-3 col-lg-3 col-md-6 col-sm-12">
+                  <div class="contact">
+                     <h3>conatct us</h3>
+                     <span>123 Second Street Fifth Avenue,<br>
+                       Manhattan, New York
+                        +987 654 3210</span>
+                  </div>
+               </div>
+                 <div class="col-xl-3 col-lg-3 col-md-6 col-sm-12">
+                  <div class="contact">
+                     <h3>ADDITIONAL LINKS</h3>
+                     <ul class="lik">
+                         <li> <a href="#">About us</a></li>
+                         <li> <a href="#">Terms and conditions</a></li>
+                         <li> <a href="#">Privacy policy</a></li>
+                         <li> <a href="#">News</a></li>
+                          <li> <a href="#">Contact us</a></li>
+                     </ul>
+                  </div>
+               </div>
+                 <div class="col-xl-3 col-lg-3 col-md-6 col-sm-12">
+                  <div class="contact">
+                     <h3>service</h3>
+                      <ul class="lik">
+                    <li> <a href="#"> Data recovery</a></li>
+                         <li> <a href="#">Computer repair</a></li>
+                         <li> <a href="#">Mobile service</a></li>
+                         <li> <a href="#">Network solutions</a></li>
+                          <li> <a href="#">Technical support</a></li>
+                  </div>
+               </div>
+                 <div class="col-xl-3 col-lg-3 col-md-6 col-sm-12">
+                  <div class="contact">
+                     <h3>IT NEXT THEME</h3>
+                     <span>Tincidunt elit magnis <br>
+                     nulla facilisis. Dolor <br>
+                  sagittis maecenas. <br>
+               Sapien nunc amet <br>
+            ultrices, </span>
+                  </div>
+               </div>
+            </div>
+         </div> -->
+            <div class="copyright">
+               <p>YourArchi.id 2021</p>
+            </div>
+         
+      </div>
+      </footr>
+      <!-- end footer -->
+      <!-- Javascript files--> 
+      <script src="js/jquery.min.js"></script> 
+      <script src="js/popper.min.js"></script> 
+      <script src="js/bootstrap.bundle.min.js"></script> 
+      <script src="js/jquery-3.0.0.min.js"></script> 
+      <script src="js/plugin.js"></script> 
+      <!-- sidebar --> 
+      <script src="js/jquery.mCustomScrollbar.concat.min.js"></script> 
+      <script src="js/custom.js"></script>
+      <script src="https:cdnjs.cloudflare.com/ajax/libs/fancybox/2.1.5/jquery.fancybox.min.js"></script>
+      <script>
+         $(document).ready(function(){
+         $(".fancybox").fancybox({
+         openEffect: "none",
+         closeEffect: "none"
+         });
+         
+         $(".zoom").hover(function(){
+         
+         $(this).addClass('transition');
+         }, function(){
+         
+         $(this).removeClass('transition');
+         });
+         });
+         
+      </script> 
+   </body>
+</html>
